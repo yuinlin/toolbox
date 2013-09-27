2cb015f49488234f26267ee9203ffc6117d4fe92
Yuin Lin <yuin.lin@aquaticinformatics.com>
2013-09-27 12:25:51 -0700

    edited line endings

--- sqlscripts/login.sql
+++ sqlscripts/login.sql
@@ -1,42 +1,20 @@
 REM dont printout anything when logging in
-
 set termout off
 
-
-
 REM set textpad as sql*Plus default editor
-
 define _editor=vi
 
-
-
 REM set serveroutput on to max size, and wrapped to preserve leading whitespace
-
 set serveroutput on size 1000000 format wrapped
 
-
-
 REM set linesize
-
 set linesize 200
 
-
-
-
 REM show user and db in prompt
-
 set sqlprompt "_user'@'_connect_identifier _date> "
 
-
-
 REM alter session set nls_date_format = 'mm/dd/rr hh24:mi';
-
-
 alter session set nls_date_format = 'mm/dd/rr hh24:mi';
 
-
-
 REM re-enable print to screen
-
 set termout on
-
