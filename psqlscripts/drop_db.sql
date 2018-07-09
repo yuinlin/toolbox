4513a98fad6170e41201a1511721d666e699ecda
yuinlin <yuin.lin@aquaticinformatics.com>
2018-07-09 14:35:20 -0700

    psqlscripts

---
+++ psqlscripts/drop_db.sql
@@ -0,0 +1,10 @@
+\set ON_ERROR_STOP on 
+
+\pset tuples_only on
+\prompt 'db to drop: ' drop_db
+
+select 'alter database ' || :'drop_db' ||' owner to '||current_user||';';
+select 'drop database ' || :'drop_db' ||';';
+select 'drop role '|| (select pg_get_userbyid(datdba) from pg_database where datname=:'drop_db') || ';';
+
+\pset tuples_only off
---
+++ psqlscripts/sessions.sql
@@ -0,0 +1,4 @@
+select pid, usename, xact_start, query_start, wait_event_type, wait_event, state, backend_xid, backend_xmin, query
+from pg_stat_activity 
+where pid <> pg_backend_pid() 
+and datname = current_database()
