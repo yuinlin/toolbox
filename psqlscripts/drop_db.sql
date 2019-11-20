545099d0a2c363a1f11ab593e87932ad69ff1750
Yuin Lin <yuin.lin@aquaticinformatics.com>
2019-11-20 11:41:59 -0800

    test

--- psqlscripts/drop_db.sql
+++ psqlscripts/drop_db.sql
@@ -3,6 +3,8 @@
 \pset tuples_only on
 \prompt 'db to drop: ' drop_db
 
+--need to test
+--select 'grant database ' || :'drop_db' ||' to '||current_user||';';
 select 'alter database ' || :'drop_db' ||' owner to '||current_user||';';
 select 'drop database ' || :'drop_db' ||';';
 select 'drop role '|| (select pg_get_userbyid(datdba) from pg_database where datname=:'drop_db') || ';';
